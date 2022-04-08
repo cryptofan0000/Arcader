34b766f9f9f90c39cbb6935ea02b556aaa396701
Roy <royaltlas9997@gmail.com>
2022-04-08 15:00:20 +0300

    update sidebar

--- client/src/components/layout/sidebar.js
+++ client/src/components/layout/sidebar.js
@@ -1,12 +1,75 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
+
+import IMG_MY_AVATAR from '../../assets/images/games/my-avatar.png'
+import IMG_METAMASK from '../../assets/images/icons/metamask-ico-sm.svg'
+import IMG_AWARD from '../../assets/images/icons/award.svg'
+
+import IMG_CS_ICO from '../../assets/images/icons/cs-icon.svg'
+import IMG_FT_ICO from '../../assets/images/icons/fgame-ico.svg'
+import IMG_CO_ICO from '../../assets/images/icons/dgame-ico.svg'
+import IMG_SP_ICO from '../../assets/images/icons/coin-ico.svg'
+
+import IMG_STAKING_ICO from '../../assets/images/icons/mobile-staking-ico.svg'
+import IMG_REFERAL_ICO from '../../assets/images/icons/mobile-referal-ico.svg'
+import IMG_LEADER_ICO from '../../assets/images/icons/mobile-leader-ico.svg'
+import IMG_NEWS_ICO from '../../assets/images/icons/mobile-news-ico.svg'
 
 const Sidebar = () => {
+	const [newsCount, setnewsCount] = useState(20)
+
 	return (
 		<Fragment>
 			<div className='sidebar-section'>
 				<div className='myinfo-section'>
 					<div className='my-avatar'>
-						
+						<img src={IMG_MY_AVATAR} alt='my-avatar' />
+					</div>
+					<div className='my-info'>
+						<span className='my-info-name'>SuperJetPack</span>
+						<span className='my-info-balance'><img src={IMG_METAMASK} alt='metamask' />3.54&nbsp;BNB</span>
+					</div>
+					<div className='my-archieve'>
+						<div className='archieve'>
+							<span>$ARD 2,023</span>
+							<span>Total earnings</span>
+						</div>
+						<div className='match-card-line'></div>
+						<div className='archieve'>
+							<span><img src={IMG_AWARD} alt='award-img' />32</span>
+							<span>Win matches</span>
+						</div>
+					</div>
+				</div>
+
+				<div className='matches-section'>
+					<div className='mobile-menu-items'>
+						<div className='mobile-menu-item'>
+							<div>
+								<img src={IMG_STAKING_ICO} alt='staking-ico' className='mobile-menu-ico' />
+								<span>Staking</span>
+							</div>
+						</div>
+						<div className='mobile-menu-item'>
+							<div>
+								<img src={IMG_REFERAL_ICO} alt='referal-ico' className='mobile-menu-ico' />
+								<span>Referal</span>
+							</div>
+						</div>
+						<div className='mobile-menu-item'>
+							<div>
+								<img src={IMG_LEADER_ICO} alt='leader-ico' className='mobile-menu-ico' />
+								<span>Leaderboard</span>
+							</div>
+						</div>
+						<div className='mobile-menu-item'>
+							<div>
+								<img src={IMG_NEWS_ICO} alt='news-ico' className='mobile-menu-ico' />
+								<span>News</span>
+							</div>
+							{
+								newsCount > 0 ? <div className='news-count-section'>{newsCount}</div> : <Fragment></Fragment>
+							}
+						</div>
 					</div>
 				</div>
 			</div>
