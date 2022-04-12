1ae3a161b4726ba5d6634fac1e93fec793b934e4
roy <royaltlas9997@gmail.com>
2022-04-12 21:46:04 +0300

    my profile page header mobile responsive added

--- client/src/components/profile/profileheader.js
+++ client/src/components/profile/profileheader.js
@@ -7,6 +7,7 @@ import IMG_TELEGR_ICO from '../../assets/images/icons/profile-telegram-ico.svg'
 import IMG_INSTAG_ICO from '../../assets/images/icons/profile-instagram-ico.svg'
 import IMG_YOUTUB_ICO from '../../assets/images/icons/profile-youtube-ico.svg'
 import IMG_PENCIL_ICO from '../../assets/images/icons/pencil-ico.svg'
+import IMG_LOGOUT_ICO from '../../assets/images/icons/logout-ico.svg'
 
 const RESOURCE_URL = 'resources/images/'
 
@@ -72,6 +73,50 @@ const ProfileHeader = ({headerData}) => {
                         <span>{headerData.maxBetsize}</span>
                         <span>Max bet size</span>
                     </div>
+                </div>
+
+                
+                <div className='mobile-profile-benefit-section'>
+                    <div className='profile-benefit'>
+                        <span>{headerData.totalEarning}</span>
+                        <span>Total earnings</span>
+                    </div>
+                    <div className='profile-vt-line'></div>
+                    <div className='profile-benefit'>
+                        <span>{headerData.totalLose}</span>
+                        <span>Total loses</span>
+                    </div>
+                    <div className='profile-vt-line'></div>
+                    <div className='profile-benefit'>
+                        <span>{headerData.maxBetsize}</span>
+                        <span>Max bet size</span>
+                    </div>
+                </div>
+
+                <div className='mobile-profile-benefit-section'>
+                    <div className='profile-benefit'>
+                        <span>{headerData.accuracy}</span>
+                        <span>Accuracy</span>
+                    </div>
+                    <div className='profile-vt-line'></div>
+                    <div className='profile-benefit'>
+                        <span>{headerData.avgBetsize}</span>
+                        <span>Average bet size</span>
+                    </div>
+                </div>
+
+                <div className='mobile-profile-social'>
+                    <a href='#'><img src={IMG_STREAM_ICO} alt='icon' className='profile-social-links' /></a>
+                    <a href='#'><img src={IMG_FORTUN_ICO} alt='icon' className='profile-social-links' /></a>
+                    <a href='#'><img src={IMG_BATTLE_ICO} alt='icon' className='profile-social-links' /></a>
+                    <a href='#'><img src={IMG_TELEGR_ICO} alt='icon' className='profile-social-links' /></a>
+                    <a href='#'><img src={IMG_YOUTUB_ICO} alt='icon' className='profile-social-links' /></a>
+                    <a href='#'><img src={IMG_INSTAG_ICO} alt='icon' className='profile-social-links' /></a>
+                </div>
+
+                <div className='exit-account'>
+                    <img src={IMG_LOGOUT_ICO} alt='icon' />
+                    <span>Exit Account</span>
                 </div>
             </div>
         </Fragment>
